91fbfc6a4be5aa6f382ddc664a32817986e9fa37
Cchiwai <Chtan@s.sfusd.edu>
2026-03-25 14:30:12 -0700

    Checkpoint 4

---
+++ Scripts/camera_shake.gd
@@ -0,0 +1,21 @@
+extends Camera2D
+
+var intensity : float = 0.0
+
+func _ready (): 
+	get_parent().OnUpdateHealth.connect(_damage_shake)
+
+func _damage_shake (health : int):
+	intensity = 10
+
+func _process (delta):
+	if intensity > 0.0:
+		intensity = lerp(intensity, 0.0, delta * 10)
+		offset = _get_random_offset()
+
+
+func _get_random_offset () -> Vector2:
+	var x = randf_range(-intensity, intensity)
+	var y = randf_range(-intensity, intensity)
+	return Vector2(x, y)
+	
